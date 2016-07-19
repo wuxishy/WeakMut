0e63db01345052c4f2b621f4add0aa61ae0c6738
Haoyuan <wuxidavid@hotmail.com>
2016-07-19 10:45:33 -0400

    oops forgot to "git add"

---
+++ loc/mutants/AOIU_4.java
@@ -0,0 +1,25 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.lang.Math;
+
+
+public class loc
+{
+
+    public  int calc( double a, double b, double c )
+    {
+        if (a < 0 || b < 0 || c < 0) {
+            return -1;
+        } else {
+            if (a + b < c || a + c < b || b + c < a) {
+                return -1;
+            } else {
+                double tmp = Math.pow( a, 2 ) + Math.pow( b, 2 ) - Math.pow( c, 2 );
+                double cos = tmp / (2 * a * -b);
+                return (int) (Math.acos( cos ) / Math.PI * 180 + 0.5);
+            }
+        }
+    }
+
+}
---
+++ mode/mutants/AOIU_2.java
@@ -0,0 +1,28 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.util.Arrays;
+
+
+public class mode
+{
+
+    public  int run( int[] arr )
+    {
+        Arrays.sort( arr );
+        int max = 1;
+        int cur = arr[0];
+        int count = 1;
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i] == cur) {
+                count += 1;
+            } else {
+                cur = arr[i];
+                count = 1;
+            }
+            max = count > max ? -count : max;
+        }
+        return max;
+    }
+
+}
